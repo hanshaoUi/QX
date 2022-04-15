cea9fcfd5615f35c13b0d9daa3272c35ba8e3389
hanshaoUi <54573836+hanshaoUi@users.noreply.github.com>
2022-04-15 17:29:15 +0800

    Update sishe-vip.js

--- pojie/sishe-vip.js
+++ pojie/sishe-vip.js
@@ -1,5 +1,3 @@
-#SSA丝社
-#下载地址：https://apps.apple.com/cn/app/ssa%E4%B8%9D%E7%A4%BE-%E5%8E%9F%E5%88%9B%E5%BD%B1%E5%83%8F/id1530892845
 
 var body = $response.body;
 var url = $request.url;
@@ -8,7 +6,7 @@ const vip = '/cms/Appapi/username/username';
 if (url.indexOf(vip) != -1) {
     obj.point= 999999;
     obj.jomkenylv = 99;
-    obj.groupname = "白金会员";
+    obj.groupname = "大嫖客";
     obj.overduedate = 2027383523;
     obj.overduedate2 = 2027383523;
     body = JSON.stringify(obj);
