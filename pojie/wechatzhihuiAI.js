3f9a64257c15ca27ba29842de863c35cba65421e
hanshaoUi <54573836+hanshaoUi@users.noreply.github.com>
2023-07-23 11:13:45 +0800

    Create wechatzhihuiAI.js

---
+++ pojie/wechatzhihuiAI.js
@@ -0,0 +1,30 @@
+/*
+微信：HOPE-RP
+微信小程序：智绘AI
+**********************
+[riwrute_local]
+^https:\/\/aituhui.top\/smallapplet\/login.* url script-response-body https://raw.githubusercontent.com/hanshaoUi/QX/main/pojie/wechatzhihuiAI.js
+[mitm]
+hostname = aituhui.top
+**********************
+
+*/
+
+let Premium = $response.body;
+var modified = JSON.parse(Premium);
+console.log(JSON.stringify(modified))
+
+function recursiveReplace(obj, key, newValue) {
+    if (obj.hasOwnProperty(key)) {
+        obj[key] = newValue;
+    }
+    for (let i in obj) {
+        if (obj[i] !== null && typeof obj[i] === 'object') {
+            recursiveReplace(obj[i], key, newValue);
+        }
+    }
+}
+
+recursiveReplace(modified, 'maxImg', 100);
+
+$done({ body: JSON.stringify(modified) });
