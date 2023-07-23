de27e89e725959d0c7b772dee75668a05c8c36dc
hanshaoUi <54573836+hanshaoUi@users.noreply.github.com>
2023-07-23 12:05:57 +0800

    Update haoqing.js

--- pojie/haoqing.js
+++ pojie/haoqing.js
@@ -3,7 +3,7 @@
 微信小程序：智绘AI
 **********************
 [riwrute_local]
-^https:\/\/chat.haoqingkeji.com\/web.php\/h5\/getBalance.* url script-response-body https://raw.githubusercontent.com/hanshaoUi/QX/main/pojie/haoqing.js
+^https:\/\/chat.haoqingkeji.com\/api.php\/user\/getInfo.* url script-response-body https://raw.githubusercontent.com/hanshaoUi/QX/main/pojie/haoqing.js
 [mitm]
 hostname = chat.haoqingkeji.com
 **********************
@@ -27,8 +27,9 @@ function recursiveReplace(obj, key, newValue) {
 
 recursiveReplace(modified, 'errno', 0);
 
-recursiveReplace(modified, 'balance', 10);
 recursiveReplace(modified, 'balance_draw', 10);
-recursiveReplace(modified, 'balance_model4', 10);
+
+recursiveReplace(modified, 'is_commission', 1);
 recursiveReplace(modified, 'vip_expire_time', '9999-9-9');
+recursiveReplace(modified, 'is_check', 0);
 $done({ body: JSON.stringify(modified) });
